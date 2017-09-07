6b4e476095a124cd29331659d93230ce2160e4b7
franklenoir <franklenoir@users.noreply.github.com>
2017-09-06 22:53:03 -0500

    Adding Name To Parts
    Adding Name To Parts

--- Inclass1.m
+++ Inclass1.m
@@ -4,27 +4,31 @@
 % the second class. Answer questions in comments and return assignment via
 % github
 %% numbers vs strings
+% Walter Frank Lenoir
 % Run the following two sections. explain why one gives a sensible result and the other doesn't
 % Can you explain the value of the variable b? 
+
 x = 1; 
-y = x + x
-%%
+y = x + x;
+%
 a = '1';
-b = a+a
+b = a+a;
 
 % x is set as an integer, where as a is set as a string or rather a char.
 % When added together, b is reading char + char instead of int + int. 
 
 %% concatenation
+% Walter Frank Lenoir
 % what is being done by the following statements? what is the difference between x2 and a2? 
 % what is the difference between x2 and x2b (hint run the size command on
 % these arrays and explain the results). 
 
-x2 = [x x]
+x2 = [x x];
 
-a2 = [a a]
+a2 = [a a];
 
-x2b = [x; x]
+x2b = [x; x];
+
 
 %x2 is creating an array of two numbers (1x2 double). %a2 is concatenating
 %two strings (a twice) together. x2 and x2b are two cell arrays with the
@@ -32,14 +36,16 @@ x2b = [x; x]
 %rows and one column.
 
 %% = vs ==, isequal
+% Walter Frank Lenoir
 % explain why the identical commands with the == commands give different answers 
 % hint: check the values of a and b as you execute the following statements
 % if necessary. 
 a = 1; b =2; 
 
-a == b
+a == b;
 a = b;
-a == b
+a = b;
+
 
 %The first logical is saying a == b is false because a = 1, and b = 2
 %(which is true). The second logical is saying a == b is true because in
@@ -48,7 +54,8 @@ a == b
 %explain the results of the following operations:
 a = [1, 2, 4];
 b = [0,2,7];
-a == b
+a == b;
+
 
 % a and b are two seperate cell arrays. a == b checks to see if a and b are
 % identical across in each corresponding cell. a == b returns a logical
@@ -57,19 +64,23 @@ a == b
 %explain why this gives an error:
 
 b = [2, 3];
-a == b
+a == b;
+
 
 %a is still set to [1, 2, 4], and b is [2, 3]. Because the arrays have
 %differing dimensions they can not be logically compared. 
 
 %explain why this does not give an error
 b = 2;
-a == b 
+a == b;
+
 
 % b is now set to an int. b is compared to each cell in the array, and can
 % return a logical array of [0,1,0]. 
 
 %%  : operator 
+% Walter Frank Lenoir
+
 %(hint for the following type "help colon" at the command line to see
 % how the ":" operator works
 % the following makes an array of the numbers from one to ten: 
@@ -77,8 +88,8 @@ oneToTen = 1:10;
 
 %here are two ways to make the array of numbers from 1 to twenty
 %explain how the second line works.
-1:20
-[oneToTen, oneToTen+10]
+1:20;
+[oneToTen, oneToTen+10];
 
 %oneToTen will create an array of numbers from 1 to 10. oneToTen+10 will
 %create a second array of numbers of 1 to 10 + 10, or 11:20. The order of
@@ -89,11 +100,14 @@ oneToTen = 1:10;
 
 % 1. even numbers from two to twenty
 2:2:20
+oneToTen * 2
 % 2. odd numbers from one to nineteen
 1:2:20
+(oneToTen * 2) - 1
 % 3. Even numbers in reverse order from twenty to two (hint lookup the
 % matlab function fliplr)
 fliplr(2:2:20)
+fliplr(oneToTen)*2;
 % explain why the fliplr funciton no longer works and find a different
 % matlab function that performs an equivalent function on the same array
 oneToTen = oneToTen';
@@ -106,6 +120,7 @@ flip(oneToTen)
 % This will reverse the order of the rows. 
 
 %% matrices, . operator
+% Walter Frank Lenoir
 
 %look at the matrix this defines. Note the other functions zeros, eye, and
 %rand which can define matrices. 
@@ -118,18 +133,21 @@ m1*m2
 m1.*m2
 
 %Without the period, normal matrix multiplication is being conducted. With
-%the period, each cell is being multiplied against each corresponding cell
+%the period, each cell in m1 is being multiplied against each corresponding cell
+%in m2.
 
 %explain this command:
 m2./m1
-%Each cell is being divided against each corresponding cell
+
+%Each cell in m2 is being divided against each corresponding cell in m1. 
 
 %extra credit: explain why this command gives a warning:
 m2/m1
 
-%determinant is 0, and inverse of matrix doesn't exist. 
+%m2/m1 requires determinant, which is 0. Therefore the inverse of matrix doesn't exist. 
 
 %% access particular elements and groups of elements
+% Walter Frank Lenoir
 
 %define a random matrix
 
